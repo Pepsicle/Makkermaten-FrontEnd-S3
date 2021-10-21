a14b3fabf08490c63ff5626d7134f0b83b2e03e2
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-10-21 11:52:56 +0200

    Update BarChart.vue

--- src/components/charts/BarChart.vue
+++ src/components/charts/BarChart.vue
@@ -2,6 +2,20 @@
 	<div class="chart">
 		<Chart :chartData="chartData" :chartOptions="chartOptions" :chartType="chartType" />
 	</div>
+  <h1>{{ this.msg }}</h1>
+  <button v-on:click="this.datasets = [{
+            label: 'asdfasdf',
+            data:  [0, 100, 10, 40, 39, 80, 40],
+            backgroundColor: '#5cddff',
+            hoverBackgroundColor: '#5cddff'
+          },
+          {
+            label: 'asgfasdfasd',
+            data:  [20, 10, 12, 33, 22, 4, 0],
+            backgroundColor: '#dddddd',
+            hoverBackgroundColor: '#dddddd'
+          }] ">Save</button>
+          <button v-on:click="this.renderChart()">refresh</button>
 </template>
 
 <script>
@@ -12,6 +26,7 @@ export default {
 	},
 	data() {
 		return {
+      msg: "test",
 			chartType: "bar",
 			chartData: {
         labels: ["Jan1", "Jan2", "Jan3", "Jan4", "Jan5", "Jan6", "Jan7"],
