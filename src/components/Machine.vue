26bf788605d6b86011e3e5c5d0963a8afd30761f
Jesper <jesper.jansen@student.fontys.nl>
2021-11-04 15:43:26 +0100

    MAchine Componenten aan makan

---
+++ src/components/Machine.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="components">
+    <h1>Send data to component</h1>
+    <ul id="array rendering">
+        <li v-for="data in items" v-bind:key="data.message">
+            <MachineComponent v-bind:componentdata="data" />
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+//import { computed } from '@vue/reactivity'
+import MachineComponent from './Component.vue'
+export default {
+  name: 'HelloWorld',
+  components:{
+      MachineComponent,
+  },
+  data(){
+      return {
+          items:[{data:[{message: 'hello'},{ count: 3}]},{data:[{message: 'goodbey'},{ count: 3}]}]
+      }
+  }
+}
+</script>
