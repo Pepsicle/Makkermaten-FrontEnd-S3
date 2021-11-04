93148d7b173b70d3d996cefdef6974f83c9e557d
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-11-04 14:32:38 +0100

    Working version

--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -2,50 +2,23 @@
 	<div>
 		<canvas class ="chartLocation"></canvas>
 	</div>
-	<!-- <div class="chart"> -->
-        <!-- <a>LINECHART TEST</a> -->
-		<!-- <newLineChart v-if="loaded" :chartdata="chartdata" :options="options"/> -->
-		<!-- <LineChartData ref="childData" /> -->
-		<!-- <Chart v-if="renderComponent" :chartData="chartData" :chartOptions="chartOptions" :chartType="chartType" /> -->
-		<!-- <button @click="logData()">Log data</button>
-		<button @click="renderGraph()">Render Graph1</button> -->
-	<!-- </div> -->
 </template>
 
 <script>
 import Chart from 'chart.js'
-//import LineChartData from './LineChartData.vue'
 
 export default {
   name: 'LineChartContainer',
-  components: {
-	//LineChartData
-	},
   methods: {
 	chartConstructor(chartType, chartData, chartOptions) {
 		const chartElement = document.getElementsByClassName("chartLocation");
-		// console.log(chartElement)
-		// console.log(chartType)
-		// console.log(chartData)
-		// console.log(chartOptions)
 		const chart = new Chart(chartElement, {
 			type: chartType,
 			data: chartData,
 			options: chartOptions,
 		});
 		return chart;
-	},
-	// receiveData(data){
-	// 	if (data != null){
-	// 		this.chartData = data;
-	// 		this.loaded = true;
-	// 		this.chartConstructor(this.chartType, this.chartdata, this.chartOptions)
-	// 		console.log("Data received!");
-	// 		console.log(this.chartData);
-	// 	} else {
-	// 		console.log("Error receiving data");
-	// 	}
-	// }
+	}
   },
   props: {
 	chartdata: {
@@ -62,15 +35,10 @@ export default {
   mounted () {
     this.loaded = false
     try {
-		//this.chartdata = this.$refs.childData.getData();
-		// console.log("THIS")
-		// console.log(this.$refs.childData.getData())
-		// console.log(datalist)
-		//console.log(this.chartdata)
 		this.loaded = true
 		this.chartConstructor(this.chartType, this.chartdata, this.options)
     } catch (e) {
-      //console.error(e)
+      console.error(e)
     }
   }
 }
