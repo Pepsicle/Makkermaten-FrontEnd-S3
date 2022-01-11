1920d496c27ff5481cb981eb24055aabc2e2140d
Randy Barents <33863457+randybarents@users.noreply.github.com>
2022-01-11 12:00:46 +0100

    BeginPlanMaintenance

--- src/components/Component.vue
+++ src/components/Component.vue
@@ -3,12 +3,30 @@
     <div v-if="componentName != ''" class="card">
       <h3 class="card-title componentTitle">{{ this.loadedComponent[0] }}</h3>
       <div class="card-body">
-        <div v-if="!shotcountloaded">Total shot count:
-          <div class="spinner spinner-border spinner-border-sm" role="status" aria-hidden="false"/>
+        <div v-if="!shotcountloaded">
+          Total shot count:
+          <div
+            class="spinner spinner-border spinner-border-sm"
+            role="status"
+            aria-hidden="false"
+          />
         </div>
-        <p v-if="shotcountloaded">Total shot count: {{this.totalShotcountcomponent}}</p>
+        <p v-if="shotcountloaded">
+          Total shot count: {{ this.totalShotcountcomponent }}
+        </p>
         <h4>Machine history:</h4>
-        <h5 v-for="machine in machines" :key="machine.omschrijving">Machine: {{machine.omschrijving}}</h5>
+        <h5 v-for="machine in machines" :key="machine.omschrijving">
+          Machine: {{ machine.omschrijving }}
+        </h5>
+        <button type="button" class="btn" @click="showModal">
+          Plan Maintenance
+        </button>
+        <MaintenanceModal v-show="isModalVisible" @close="closeModal">
+          <template v-slot:header> New maintenance. </template>
+
+          <template v-slot:body><MaintenanceTemp></MaintenanceTemp></template>
+
+        </MaintenanceModal>
       </div>
     </div>
   </div>
@@ -16,30 +34,45 @@
 
 <script>
 import MachineDataService from "../Service/MachineDataService";
-import ComponentDataService from "../Service/ComponentDataService"
+import ComponentDataService from "../Service/ComponentDataService";
+import MaintenanceTemp from "../components/MaintenanceTemp.vue"
+import MaintenanceModal from "../components/MaintenanceModal.vue";
 
 export default {
   name: "component",
+  components: {
+    MaintenanceTemp,
+    MaintenanceModal,
+  },
   data: () => ({
     shotcountloaded: false,
     machines: [],
     componentName: "",
     totalShotcountcomponent: "",
+    isModalVisible: false,
   }),
   methods: {
     async GetMachinesByComponent() {
       var temp = await MachineDataService.GetMachineHistoryFromComponent(
         this.componentName
       );
-      console.log(temp)
+      console.log(temp);
       this.machines = temp.data;
     },
     async GetTotalShotcount() {
-      this.shotcountloaded = false
-      var temp = await ComponentDataService.GetTotalshotCountFromComponent(this.componentName)
-      console.log(temp.data)
-      this.totalShotcountcomponent = temp.data
-      this.shotcountloaded = true
+      this.shotcountloaded = false;
+      var temp = await ComponentDataService.GetTotalshotCountFromComponent(
+        this.componentName
+      );
+      console.log(temp.data);
+      this.totalShotcountcomponent = temp.data;
+      this.shotcountloaded = true;
+    },
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
     },
   },
   props: {
@@ -48,14 +81,13 @@ export default {
       type: Object,
     },
   },
-  watch:{
-      loadedComponent : function()
-      {
-        this.componentName = this.loadedComponent[0].split(" -")[0]
-        console.log(this.componentName)
-        this.GetTotalShotcount()
-        return this.GetMachinesByComponent(this.componentName)
-      }
+  watch: {
+    loadedComponent: function () {
+      this.componentName = this.loadedComponent[0].split(" -")[0];
+      console.log(this.componentName);
+      this.GetTotalShotcount();
+      return this.GetMachinesByComponent(this.componentName);
+    },
   },
 };
 </script>
@@ -63,5 +95,16 @@ export default {
 <style scoped>
 .componentTitle {
   padding-left: 1.5%;
+}
+
+.btn {
+  background-color: #4b66c5;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 </style>
--- src/components/MaintenanceTemp.vue
+++ src/components/MaintenanceTemp.vue
@@ -1,6 +1,4 @@
 <template>
-    <br><br>
-    <p>Maintenance TEMP</p>
     <div class="card col-md-4 offset-4">
         <label for="start">Maintenance Date: </label><br>
         <input v-model="selectedDateTime" type="datetime-local" id="start" name="Maintenance Time" >
@@ -11,16 +9,14 @@
             <option value="Predictive Maintenance" >Predictive Maintenance</option>
             <option value="Corrective Maintenance">Corrective Maintenance</option>
             <option value="Predetermined Maintenance">Predetermined Maintenance</option>
-          </select>
-    </div>
-    <div class="card" style="width: 50%; margin: 1%;">
-        <p>Maintenance Entered:</p>
-        <p>Start Date: {{this.selectedDateTime}}</p>
-        <p>Maintenance Type: {{this.maintenanceDescription}}</p>
+        </select>
+        <button type="button" class="btn" @click="PlanMaintenance">Confirm maintenance</button>
     </div>
 </template>
 
 <script>
+import MaintenanceDataService from "../Service/MaintenaceDataService";
+
 
 export default {
     name: "Maintenance",
@@ -31,6 +27,10 @@ export default {
         maintenanceType: '',
     }),
     methods: {
+        async PlanMaintenance()
+        {
+            await MaintenanceDataService.CreateNewMaintenance(this.selectedDateTime , this.maintenanceType)
+        }
     },
     async mounted() {
     },
