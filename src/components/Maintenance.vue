60a4c5016e49b2c46848c43b5ffc8482d10589d3
Randy Barents <33863457+randybarents@users.noreply.github.com>
2022-01-13 10:32:38 +0100

    Maintenance Table Creation

---
+++ src/components/Maintenance.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="infotable">
+  <table border=10 class="datatable">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Maintenance Type</th>
+        <th>Planned Date</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in res.data" :key="item.componentName">
+      <td>{{item.componentName}}</td>
+      <td>{{item.maintenanceType}}</td>
+      <td>{{item.dayOfMaintenance}}</td>
+      <td>{{item.description}}</td>
+    </tr>
+    </tbody>
+  </table>
+  </div>
+</template>
+
+<script>
+import MaintenanceDataService from "../Service/MaintenaceDataService";
+
+export default {
+  name: "Maintenance",
+  components: {},
+  data: () => ({
+    res: "",
+  }),
+  mounted() {
+    this.GetMaintenances();
+  },
+  methods: {
+    async GetMaintenances() {
+      this.res = await MaintenanceDataService.GetAllMaintenance();
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
